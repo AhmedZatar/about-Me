5d6e198c7dd8cf049ebec8e49b52e6fb2caad67b
ahmedzatar <ahmed.zatar2@gmail.com>
2021-03-07 12:35:26 +0200

    add alert with the possible correct answers in app.js

--- js/app.js
+++ js/app.js
@@ -100,6 +100,7 @@ function question7(q){
 question7(ar[7]);
 if(a7===0){
   alert('You could not guess the video games that Ahmed liked');
+  alert('the possible correct answers is : rust, minecraft, rome total war, battlefield 4, battlefield 1, grand theft auto v, league of legends');
 }
 alert('you will see all the possible correct answers in the main page');
 
